2a816d3900b3ff91a047a1c388b0aa326cc3244f
Jerms Buganan <liljerms@gmail.com>
2019-08-31 23:48:12 -0700

    change rotation system

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -4,55 +4,54 @@ let shape = {
   top: 150,
   left: 300,
   moveUnit: 15,
-  rotationUnit: 90
+  rotationIndex: 1,
+  type: ''
 };
 
-// Get shape
-let shape1 = document.querySelector('#shape1');
+// Stats
 let xPos = document.querySelector('#xPos');
 let yPos = document.querySelector('#yPos');
+
+// Get current shape and set initial position
+let currentShape = document.querySelector('#currentShape');
+setXandY(shape.left, shape.top);
+
+// Listen for keydown
 window.addEventListener('keydown', transformShape);
-setXandY();
 
 function transformShape(e) {
   switch(e.keyCode) {
-    case 88: // Rotate Left
-      rotateShape(shape.degrees += shape.rotationUnit);
+    case 88: // Rotate Right
+      currentShape.classList.remove(`rot-${shape.rotationIndex}`);
+      shape.rotationIndex = (shape.rotationIndex === 4) ? 1 : shape.rotationIndex + 1;
+      currentShape.classList.add(`rot-${shape.rotationIndex}`);
       break;
-    case 90: // Rotate Right
-      rotateShape(shape.degrees -= shape.rotationUnit);
+    case 90: // Rotate Left
+      currentShape.classList.remove(`rot-${shape.rotationIndex}`);
+      shape.rotationIndex = (shape.rotationIndex === 1) ? 4 : shape.rotationIndex - 1;
+      currentShape.classList.add(`rot-${shape.rotationIndex}`);
       break;
     case 37: // Move Left
       shape.left -= (shape.left <= 0) ? 0 : shape.moveUnit;
-      moveShape('Left', shape.left);
       break;
     case 38: // Move Up
       shape.top -= (shape.top <= 0) ? 0 : shape.moveUnit;
-      moveShape('Top', shape.top);
       break;
     case 39: // Move Right
       shape.left += (shape.left >= (window.visualViewport.width - shape.moveUnit * 3)) ? 0 : shape.moveUnit;
-      moveShape('Left', shape.left);
       break;
     case 40: // Move Down
       shape.top += (shape.top >= (window.visualViewport.height - shape.moveUnit * 3)) ? 0 : shape.moveUnit;
-      moveShape('Top', shape.top);
       break;
     default:
       break;
   }
-  setXandY();
+  setXandY(shape.left, shape.top);
 }
 
-function rotateShape(degrees) {
-  document.documentElement.style.setProperty(`--shapeDegrees`, `${degrees}deg`);
-}
-
-function moveShape(origin, newPos) {
-  document.documentElement.style.setProperty(`--shape${origin}`, `${newPos}px`);
-}
-
-function setXandY() {
-  xPos.innerHTML = shape.left;
-  yPos.innerHTML = shape.top;
+function setXandY(x, y) {
+  currentShape.style.left = `${x}px`;
+  currentShape.style.top = `${y}px`;
+  xPos.innerHTML = x;
+  yPos.innerHTML = y;
 }
